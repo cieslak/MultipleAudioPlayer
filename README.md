cf7c59a5979eaac839bcca72ea09ccf62e4364c6
Chris Cieslak <chris@electropuf.com>
2021-11-20 16:47:36 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MultipleAudioPlayer
+
+A description of this package.
---
+++ Sources/MultipleAudioPlayer/MultipleAudioPlayer.swift
@@ -0,0 +1,6 @@
+public struct MultipleAudioPlayer {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
